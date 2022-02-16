9b5c2e710e0bd25ed8b76d851f96dc89b7d272b3
MayerJuly <73027797+MayerJuly@users.noreply.github.com>
2022-02-16 11:45:31 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,10 +2,17 @@
 let croc = document.querySelector('.croc-blur')
 if(window.innerWidth>900){
     document.querySelector('.preloader').classList.add('show');
-    croc.innerHTML = '<div class="croc" data-w-id="1c5a339c-9750-5e3e-4290-8d385408a326" data-animation-type="lottie" data-src="documents/crocodile_blue.json" data-loop="0" data-direction="1" data-autoplay="0" data-is-ix2-target="1" data-renderer="svg" data-default-duration="1.9602937966296492" data-duration="0"></div>\n';
+    croc.innerHTML = '<div class="croc" data-w-id="ee0dfb00-8829-c091-ad03-92e8d1ef2e6e" data-animation-type="lottie" data-src="documents/animation.json" data-loop="0" data-direction="1" data-autoplay="0" data-is-ix2-target="1" data-renderer="svg" data-default-duration="2" data-duration="0"></div>\n';
 } else {
     croc.innerHTML = '<img src="images/background.jpg" alt="Background" class="croc-img">\n';
 }
+
+setTimeout(function(){
+    document.body.classList.add('finish');
+},400)
+
+
+
 // let requestURL = '../documents/crocodile_blue.json';
 // let request = new XMLHttpRequest();
 // request.open('GET',requestURL)
